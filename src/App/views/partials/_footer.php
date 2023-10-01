78fc636503254efae2841916b41817264dc8992a
targemac <targemac@gmail.com>
2023-10-01 10:16:42 +0100

    added partials

---
+++ src/App/views/partials/_footer.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+<footer class="container mx-auto my-12 text-center">
+    &copy; 2023 PHP Expense Tracker App
+</footer>
+<!-- End Footer -->
+</body>
+
+</html>
--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -28,14 +28,19 @@ class TemplateEngine
         // this tells php to store contents in an output buffer untill every line has fininshed loading
         ob_start();
 
-        include "{$this->basePath}/{$template}";
+        include $this->resolve($template);
 
         // this searches for an active buffer content, if found, it returns contents as a string
         $output = ob_get_contents();
 
         // stop output buffer and wipe buffer memory
         ob_end_clean();
-        
+
         return $output;
+    }
+
+    public function resolve(string $path)
+    {
+        return "{$this->basePath}/{$path}";
     }
 }
