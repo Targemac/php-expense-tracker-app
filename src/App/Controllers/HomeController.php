eeef10ddadb1d7d39df460489c8192e88c38d8b1
targemac <targemac@gmail.com>
2023-10-02 07:57:01 +0100

    validating constructor method

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -21,11 +21,11 @@ use App\Config\Paths;
 
 class HomeController
 {
-    private TemplateEngine $view;
 
-    public function __construct()
+
+    public function __construct(private TemplateEngine $view)
     {
-        $this->view = new TemplateEngine(Paths::VIEW);
+        // $this->view = new TemplateEngine(Paths::VIEW);
     }
 
 
